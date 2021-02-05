cd14064d22c492076934bbb4e17e5eb3b22917f1
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2021-02-04 19:51:05 -0500

    wires hello world component into the rails view

--- app/javascript/components/HelloWorld.test.js
+++ app/javascript/components/HelloWorld.test.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { mount } from 'enzyme';
+
+import HelloWorld from './HelloWorld.js'
+
 test("HelloWorld Component", () => {
   const wrapper = mount(<HelloWorld/>);
   expect(wrapper.contains(<div>What is 5+2?</div>)).toBe(true)
