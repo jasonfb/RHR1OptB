621340924dc13384061fedd2810cf24bcb7dd16c
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2021-02-04 19:38:46 -0500

    Jest config excludes rails config/webpacker/test.js folder; failing spec in jest HelloWorld.test.js

---
+++ spec/features/math_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe "the signin process", type: :feature do
+  it "asks me to add to two numbers" do
+    visit '/'
+    expect(page).to have_content 'What is 5+2'
+  end
+end
